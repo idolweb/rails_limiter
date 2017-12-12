259bbda05d6beec0235b6743a0325a2f50264f50
Jean-Philippe Lecaille <jpl@idol.io>
2017-12-12 11:41:42 +0100

    Status code 429 in case of limit reached

--- lib/rails_limiter/rails_limiter.rb
+++ lib/rails_limiter/rails_limiter.rb
@@ -3,10 +3,9 @@ require 'active_support/core_ext/object/blank'
 module RailsLimiter::RailsLimiter
   def limit(key, max, expire_in, url)
     valid, ttl  = process_limit(key, max, expire_in)
-    
+
     unless valid
-      flash[:error] = "You exceeded your requests limit of #{max}. Try again in #{ttl} seconds"
-      redirect_to url
+      render :status => 429, :json => {:message => "You exceeded your requests limit of #{max}. Try again in #{ttl} seconds"}
       return
     end
   end
@@ -15,17 +14,17 @@ module RailsLimiter::RailsLimiter
   def get_connexion
     @connexion  ||= RailsLimiter::Init.configuration.connexion
   end
-  
+
   def process_limit(key, max, expire_in = 1.day.seconds.to_i)
     get_connexion
-    
+
     val = @connexion.get(key)
-    
+
     if val.blank?
       # si connexion ne possède pas de valeur pour la clé, on la créée
       @connexion.set(key, 1)
       @connexion.expire(key, expire_in)
-      
+
       [true, 0]
     elsif val.to_i < max
       # si connexion possède la clé, on incrémente
--- lib/rails_limiter/version.rb
+++ lib/rails_limiter/version.rb
@@ -1,3 +1,3 @@
 module RailsLimiter
-  VERSION = "0.3.1"
+  VERSION = "0.3.3"
 end
