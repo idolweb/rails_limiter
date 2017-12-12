b89048a9e3795e1088ef431a0f1c8a8433f042b8
Jean-Philippe Lecaille <jpl@idol.io>
2017-12-12 11:37:44 +0100

    Revert "Tests against 5.0.0.beta1"
    This reverts commit 25050fa55c345d7c39b6b5a67a43c3ae670c6a36.

--- lib/rails_limiter/version.rb
+++ lib/rails_limiter/version.rb
@@ -1,3 +1,3 @@
 module RailsLimiter
-  VERSION = "0.3.2"
+  VERSION = "0.3.1"
 end
--- test/dummy/app/controllers/test_controller.rb
+++ test/dummy/app/controllers/test_controller.rb
@@ -1,5 +1,5 @@
 class TestController < ApplicationController
-  before_action :only => :index do
+  before_filter :only => :index do
     limit(
       "extract",
       2,
